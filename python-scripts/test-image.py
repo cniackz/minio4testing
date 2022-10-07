316f0a4589c0c24820e7f5441b8686277e79966f
Cesar Celis <celis.hernandez.cesar@gmail.com>
2022-10-07 15:07:48 -0400

    Download image with Python SDK

--- python-scripts/test-image.py
+++ python-scripts/test-image.py
@@ -1,1 +1,21 @@
-print("work in progress....")
+from minio import Minio
+from minio.error import S3Error
+
+
+def main():
+    # Create a client with the MinIO server playground, its access key
+    # and secret key.
+    client = Minio(
+        "play.min.io",
+        access_key="minio",
+        secret_key="minio123",
+    )
+
+    # Download image
+    print(client.fget_object("my-bucket", "image.png", "~/image.png"))
+
+if __name__ == "__main__":
+    try:
+        main()
+    except S3Error as exc:
+        print("error occurred.", exc)
