dc25423853edae21788f50d28394c0c960a3a229
Cesar Celis <celis.hernandez.cesar@gmail.com>
2022-10-13 08:21:26 -0400

    Comparing images

--- python-scripts/test-image.py
+++ python-scripts/test-image.py
@@ -17,6 +17,7 @@ def main():
     print(os.environ['GITHUB_WORKSPACE'])
     path_file = os.environ['GITHUB_WORKSPACE'] + "/ubuntu-logo.jpeg"
     path_file_resized = os.environ['GITHUB_WORKSPACE'] + "/logo-resized.jpeg"
+    logo_verify = os.environ['GITHUB_WORKSPACE'] + "/logo-verify.jpeg"
 
     # Download image
     print(client.fget_object("my-bucket", "ubuntu-logo.jpeg", path_file))
@@ -28,8 +29,20 @@ def main():
     new_image = image.resize((500, 500))
     new_image.save(path_file_resized)
 
-    # Next: Upload image to MinIO
-    # Next: Verify image was properly uploaded to MinIO
+    # Upload image to MinIO
+    client.fput_object("my-bucket", path_file_resized, "logo-resized.jpeg")
+
+    # Verify image was properly uploaded to MinIO
+    client.fget_object("my-bucket", "logo-resized.jpeg", logo_verify)
+    image_one = Image.open(path_file_resized)
+    image_two = Image.open(logo_verify)
+
+    diff = ImageChops.difference(image_one, image_two)
+
+    if diff.getbbox():
+        print("images are different")
+    else:
+        print("images are the same")
 
 if __name__ == "__main__":
     try:
